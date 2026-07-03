b3230ec3c94ff1581b09abf1292240de66060977
Haoming <hmstudy02@gmail.com>
2026-07-03 19:57:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+﻿# SD Forge Layer Debug
+This is an Extension for [Forge Neo](https://github.com/Haoming02/sd-webui-forge-classic/tree/neo), which allows you to quickly inspect the layer information of a model
+
+<p align="center">
+<img src="./ui.png">
+</p>
---
+++ scripts/layer.py
@@ -0,0 +1,46 @@
+import gradio as gr
+
+from backend.utils import load_torch_file
+from modules.script_callbacks import on_ui_tabs
+
+INFO: dict[str, str] = {}
+
+
+def _load_inner(path: str) -> tuple[dict[str, str], bool]:
+    sd = load_torch_file(path)
+    INFO.clear()
+    INFO.update({k: f"{list(v.shape)} ({v.dtype})" for k, v in sd.items()})
+    return gr.update(value=INFO, visible=True), gr.update(value=None, visible=True)
+
+
+def load(path: str):
+    try:
+        return _load_inner(path)
+    except Exception as e:
+        gr.Warning(str(e))
+        return [gr.update(value=None, visible=False)] * 2
+
+
+def on_filter(search: str):
+    return gr.update(value={k: v for k, v in INFO.items() if search in k})
+
+
+def debug_ui():
+    with gr.Blocks() as LAYER_DEBUG:
+        with gr.Row():
+            path = gr.Textbox(lines=1, max_lines=1, label="Path to Model", scale=9)
+            btn = gr.Button("Load", variant="primary", scale=1)
+
+        search = gr.Textbox(lines=1, max_lines=1, label="Filter", visible=False)
+        info = gr.JSON(value=None, label="Layer Info", visible=False)
+
+        for comp in (path, btn, search, info):
+            comp.do_not_save_to_config = True
+
+        btn.click(fn=load, inputs=[path], outputs=[info, search])
+        search.blur(fn=on_filter, inputs=[search], outputs=[info])
+
+    return [(LAYER_DEBUG, "Layer Debug", "sd-forge-layer-debug")]
+
+
+on_ui_tabs(debug_ui)
